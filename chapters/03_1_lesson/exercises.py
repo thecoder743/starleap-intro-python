3054ebefbff67e04c87ad6981d7f0045fef4cdc8
thecoder743 <thecoder743@gmail.com>
2025-09-04 13:51:27 +0000

    week 3 classwork

--- chapters/03_1_lesson/exercises.py
+++ chapters/03_1_lesson/exercises.py
@@ -4,6 +4,14 @@
 
 print("********** Ch 3 Exercise 1 **********")
 
+def right_justify(input):
+    length = len(input)
+    print("length = ", length)
+    target = 70
+    spaces = target - length
+    space_string = ' '*spaces
+    print(space_string + input)
+right_justify('Monty')
 # Do your work for Exercise 1 here
 
 print("Ch 3 Exercise 1: Not implemented") # Delete this line when you write your code!
@@ -11,6 +19,15 @@ print("Ch 3 Exercise 1: Not implemented") # Delete this line when you write your
 
 
 print("********** Ch 3 Exercise 2 **********")
+
+def do_twice(f):
+    f()
+    f()
+    f()
+def print_spam():
+    print('spam')
+
+do_twice(print_spat)
 
 # Do your work for Excercise 2 here.
 
