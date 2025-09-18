5d33ece2b106562fbccdb8644ee382940b825316
thecoder743 <thecoder743@gmail.com>
2025-09-18 13:56:14 +0000

    week 5 work

--- chapters/05_1_lesson/exercises.py
+++ chapters/05_1_lesson/exercises.py
@@ -7,18 +7,35 @@ print("********** Ch 5 Exercise 1 **********")
 import time
 time.time()
 
+def time_since():
+    import time
+    s = 60
+    m = 60
+    h = 24
+    t = time.time()
+    days = int(t // s // m // h)
+    hours = int(t // s // m)
+    minutes = int(t // s)
+    seconds = t
+    print("days = ", days, "hours =", hours,  "minutes =", minutes,  "seconds =", seconds)
 
+
+time_since()
 
 
 print("********** Ch 5 Exercise 2 **********")
 
-# Do your work for Excercise 2 here.
+def check_fermat():
+    A = input("what is the value of A? ")
+    B = input("what is the value of B ")
+    C = input("what is the value of C ")
+    N = input("what is the value of N ")
+    print(A, B, C, N)
 
-print("Ch 5 Exercise 2: Not implemented") # Delete this line when you write your code!
-
-
-
+check_fermat()
 print("********** Ch 5 Exercise 3 **********")
+
+
 
 # Do your work for Exercise 3 here.
 
